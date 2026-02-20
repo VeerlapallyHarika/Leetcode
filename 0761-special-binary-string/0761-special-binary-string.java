258a24433fad13e9b4530d70305997a84603c8a9
VeerlapallyHarika <harikareddy7241@gmail.com>
2026-02-20 23:38:52 +0530

    Time: 2 ms (100%), Space: 43.2 MB (79.62%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String makeLargestSpecial(String s) {
+        List<String> subs = new ArrayList<>();
+        int count = 0, start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') count++;
+            else count--;
+            
+            if (count == 0) {
+                String inner = s.substring(start + 1, i);
+                subs.add("1" + makeLargestSpecial(inner) + "0");
+                start = i + 1;
+            }
+        }
+        Collections.sort(subs, Collections.reverseOrder());
+        StringBuilder result = new StringBuilder();
+        for (String sub : subs) {
+            result.append(sub);
+        }
+
+        return result.toString();
+    }
+}
